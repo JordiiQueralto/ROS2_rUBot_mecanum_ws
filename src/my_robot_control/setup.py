47f7225be6a894da564b2556d7dcd42d7a07f69d
JordiiQueralto <jquerape20@alumnes.ub.edu>
2025-11-22 00:59:48 +0000

    self holonomic

--- src/my_robot_control/setup.py
+++ src/my_robot_control/setup.py
@@ -33,6 +33,7 @@ setup(
             'my_robot_go2pose_exec = my_robot_control.my_robot_go2pose:main',
             'my_robot_lidar_test_V1_exec = my_robot_control.my_robot_lidar_test_V1:main',
             'my_robot_control_lidar_exec = my_robot_control.my_robot_lidar_test_V2:main',
+            'my_robot_selfcontrol_holonomic_exec = my_robot_control.my_robot_selfcontrol_holonomic:main',
         ],
     },
 )
